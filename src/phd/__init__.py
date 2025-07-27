dcdcd8fb4cd1b98d9e2a2bec3f19bb4c57a0c586
MrTreev <mr.treev@gmail.com>
2025-07-28 09:46:06 +1000

    Mulitprocess thing

--- src/phd/__init__.py
+++ src/phd/__init__.py
@@ -1,12 +1,5 @@
 import torch
 
-# torch.multiprocessing.set_sharing_strategy("file_system")
-torch.multiprocessing.set_start_method("spawn")
-if torch.cuda.is_available():
-    dev = torch.device("cuda")
-    if dev is not None:
-        torch.set_default_device(dev)
-
 from .scripts.check import check
 from .scripts.legacy import main
 from .scripts.scratch import scratch
