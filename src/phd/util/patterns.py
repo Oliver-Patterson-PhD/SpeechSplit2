c44b231b1d83e45f34ceeea6c30b57411e0ab633
MrTreev <mr.treev@gmail.com>
2025-08-05 00:08:21 +1000

    Stuff

--- src/phd/util/patterns.py
+++ src/phd/util/patterns.py
@@ -2,18 +2,16 @@ __all__ = [
     "Singleton",
 ]
 
-from torch.multiprocessing import Lock
+import torch.multiprocessing as mp
 
 
 class Singleton(type):
     _instances: dict = {}
-    _lock = Lock()  # Ensures thread-safety for singleton instantiation
+    _lock = mp.Lock()
 
     def __call__(cls, *args, **kwargs):
-        # Critical section where instance creation happens
         with cls._lock:
             if cls not in cls._instances:
-                # If the class does not have an instance, create one
                 print(f"Creating singleton {cls.__name__}")
-                cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+                cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
